c4a15216b356a4a97f6262054da20e758c435439
Michael Ries <michael@riesd.com>
2011-09-05 12:25:26 -0600

    removing all of the old framework and starting new

--- app/models/project.rb
+++
@@ -1,8 +0,0 @@
-class Project
-  include Mongoid::Document
-  
-  field :name, type: String
-  field :date, type: Date
-  
-  has_many :images
-end
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-HistoryMedia::Application.config.secret_token = 'a0a5bb4a8103283bff295be5f1d01b2ef5204ae77c47008dbd58f9eb17833cbb20adf1eadd8f6321badb97ba547b8727173e128b11a0061b303867a954876e97'
