b6b7e0e422ee2e640adb1a404736074f24e703ae
Michael Ries <michael@riesd.com>
2011-09-05 12:26:01 -0600

    adding a brand new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FamilyAlbum::Application.config.secret_token = '91e496c74cdc25aee355fefdd11075533a3018b65443dfbac9c1a7951321ebedf6bf013434b7c4866e081e677c0051680828297eebaa09f20648899f69500a2c'
