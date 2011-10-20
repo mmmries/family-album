eed2e0a448aae0aad8a0252a0cb1f11dd0039506
Michael Ries <michael@riesd.com>
2011-10-19 23:49:17 -0600

    adding skeleton of a user management section

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 FamilyAlbum::Application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get '/login' => 'devise/sessions#new'
+    get '/logout' => 'devise/sessions#destroy'
+  end
 
   resources :albums
   resources :pictures
+  resources :user, :controller => "users"
   
   root :to => "albums#index"
 
